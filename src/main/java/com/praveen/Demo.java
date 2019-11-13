bf5859efa03afbe5506763c64f836f7ab256393d
vallurupallipraveenchowdary <vallurupallipraveen@gmail.com>
2019-11-13 13:18:31 +0530

    sub method is added

--- src/main/java/com/praveen/Demo.java
+++ src/main/java/com/praveen/Demo.java
@@ -1,21 +1,30 @@
 package com.praveen;
 
 public class Demo {
-
-	
+          
 		  void sum (int a, int b)
 		  {
 		    System.out.println("sum is"+" "+(a+b)) ;
 		  }
+		  
 		  void sum (float a, float b)
 		  {
 		    System.out.println("sum is"+" "+(a+b));
 		  }
+		  	  
+		  void sub (int a, int b)
+		  {
+		    System.out.println("sub is"+" "+(a-b)) ;
+		  }
+		  
 		  public static void main (String[] args)
 		  {
 		    Demo  myObj = new Demo();
+		    
 		    myObj.sum (8,5);      
-		    myObj.sum (4.6f, 3.8f);
+		    myObj.sum (4.6f, 3.8f); 
+		    myObj.sub (15,5);
+		    
 		  }
 		}
 	
