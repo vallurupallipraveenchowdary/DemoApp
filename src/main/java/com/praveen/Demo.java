a433ee68a3a42ce75dbe217528bbc7e8692ca80d
vallurupallipraveenchowdary <vallurupallipraveen@gmail.com>
2019-11-13 12:23:45 +0530

    DemoApp initial commit

---
+++ src/main/java/com/praveen/Demo.java
@@ -0,0 +1,22 @@
+package com.praveen;
+
+public class Demo {
+
+	
+		  void sum (int a, int b)
+		  {
+		    System.out.println("sum is"+" "+(a+b)) ;
+		  }
+		  void sum (float a, float b)
+		  {
+		    System.out.println("sum is"+" "+(a+b));
+		  }
+		  public static void main (String[] args)
+		  {
+		    Demo  myObj = new Demo();
+		    myObj.sum (8,5);      
+		    myObj.sum (4.6f, 3.8f);
+		  }
+		}
+	
+
